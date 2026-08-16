e83ef72273470048223c1344b34cb8f6597b6f43
aarish <aarishahmed96@gmail.com>
2026-08-17 02:26:05 +0600

    v2 is ready for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AuthProvider from './context/authContext.jsx'
+import AuthProvider from './context/AuthContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
